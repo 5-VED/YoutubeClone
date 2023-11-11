49f24cbde5aa9b19713bc5e7fd1e6a776114ce81
5-VED <parmarved1@gmail.com>
2023-11-12 06:12:25 +1300

    Basic Config of Project

---
+++ src/models/user.model.js
@@ -0,0 +1,54 @@
+import { Schema, model } from "mongoose"
+import bcrypt from "bcrypt"
+
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trin: true,
+        index: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trin: true
+    },
+    fullname: {
+        type: String,
+        required: true,
+        trin: true,
+        index: true
+    },
+    avatar: {
+        type: String, // Using Clouddinary URL
+        require: true
+    },
+    coverImage: {
+        type: String, // Using Clouddinary URL        
+    },
+    watchHistory: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+        }
+    ],
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    },
+    refreshToken: {
+        type: String
+    }
+
+}, {
+    timestamps: true
+})
+
+userSchema.pre()
+
+export const User = model("User", userSchema)
