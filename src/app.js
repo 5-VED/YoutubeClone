be0372c2509d3dd8bb4623b7efa34ff7d2966266
5-VED <parmarved1@gmail.com>
2023-11-22 01:45:25 +1300

    Added user routes and controller

--- src/app.js
+++ src/app.js
@@ -7,7 +7,6 @@ const app = express()
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
-
 }))
 
 app.use(express.json({ limit: "16kb" }))
@@ -17,5 +16,10 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 
 app.use(cookieParser())
+
+// Importing Routes 
+import userRouter from './routes/user.routes.js'
+
+app.use("/api/v1/user", userRouter)
 
 export { app }
