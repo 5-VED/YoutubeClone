9628d44f2ffab53537f4a237616c4ce3193b1042
5-VED <parmarved1@gmail.com>
2023-12-15 23:18:53 +1300

    Created Access and Refresh Token

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,21 @@ import { ApiResponse } from "../utils/ApiResponse.js"
 import { User } from "../models/user.model.js"
 
 
+const generateAccessTokenAndRefreshToken = async (userId) => {
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({ validateBeforeSave: false })
+        return { accessToken, refreshToken }
+
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating Refresh or Access Tokern", error)
+    }
+}
+
 
 /**
  *  @route http://localhost:3000/api/v1/user/register
@@ -52,7 +67,7 @@ const registerUser = asyncHandler(async (req, res, next) => {
 /**
  *  @route http://localhost:3000/api/v1/user/login
  *  @method POST
- *  @action Controller to register new User
+ *  @action Controller to Login User
  */
 const loginUser = asyncHandler(async (req, res, next) => {
     const { email, username, password } = req.body
@@ -68,6 +83,43 @@ const loginUser = asyncHandler(async (req, res, next) => {
     if (!user) {
         throw new ApiError(404, 'User not Found')
     }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if (!isPasswordValid) {
+        throw new ApiError(401, 'Invalid User Credentials')
+    }
+
+    const { accessToken, refreshToken } = await generateAccessTokenAndRefreshToken(user._id)
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res.status(200).
+        cookie("accessToken", accessToken, options).
+        cookie("refreshToken", refreshToken, options).
+        json(
+            new ApiResponse(200,
+                {
+                    user: loggedInUser,
+                    accessToken,
+                    refreshToken
+                },
+                "User Logged In Successfuly")
+        )
 })
 
-export { registerUser }
+
+/**
+ *  @route http://localhost:3000/api/v1/user/logout
+ *  @method POST
+ *  @action Controller to Logout User
+ */
+const logOutUser = asyncHandler(async (req,res,next) => {
+    const user = await User.find
+})
+export { registerUser, loginUser }
